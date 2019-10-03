0b336d75f88654a23ea7b50d0371d49f681ed5f6
Oscar Alvarez <oalvarez@vb4b.cc>
2019-10-03 08:15:57 -0500

    [Se agrega configuración para el envio de mensajes desde servidor gmail]

--- WS_OdiseaMail/WS_OdiseaMail/ServicioMail.svc.cs
+++ WS_OdiseaMail/WS_OdiseaMail/ServicioMail.svc.cs
@@ -5,7 +5,9 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
+using WS_OdiseaMail.Data;
 using System.Web.Services;
+using System.Configuration;
 
 namespace WS_OdiseaMail
 {
@@ -16,8 +18,42 @@ namespace WS_OdiseaMail
         {
             string msgError = "";
 
+            string smtp_server      = "";
+            string smtp_user        = "";
+            string smtp_pass        = "";
+            string smtp_puertossl   = "";
+            string smtp_ssl         = "";
+            string remitenteMAIL    = "";
 
+            try
+            {
+                smtp_server      = ConfigurationManager.AppSettings["smtp_server"].ToString();
+                smtp_user        = ConfigurationManager.AppSettings["smtp_user"].ToString();
+                smtp_pass        = ConfigurationManager.AppSettings["smtp_pass"].ToString();
+                smtp_puertossl   = ConfigurationManager.AppSettings["smtp_sslport"].ToString();
+                smtp_ssl         = ConfigurationManager.AppSettings["RequiereSSL"].ToString();
+                remitenteMAIL    = ConfigurationManager.AppSettings["remitenteMAIL"].ToString();
+            }
+            catch(Exception e)
+            {
+                msgError = "Error en la configuración del servicio.";
+            }
 
+            if (msgError.Equals(""))
+            {
+                Mail mail = new Mail(smtp_server, 
+                                     smtp_pass, 
+                                     smtp_user, 
+                                     remitenteMAIL, 
+                                     bool.Parse(smtp_ssl), 
+                                     smtp_puertossl,
+                                     destinatario,
+                                     null, 
+                                     asunto, 
+                                     mensaje);
+
+                msgError = mail.enviar();
+            }
             return msgError;
         }
     }
